274763ff8a3eb8b5975cc6f450e0a511bfb902c4
SquareRoot1 <m.kanlidere01@gmail.com>
2017-08-01 15:44:47 +0300

    fix

--- index.js
+++ index.js
@@ -124,7 +124,19 @@ function sendGenericMessage(recipientId) {
               payload: "Success for second item",
             }]
           }]
+        },
+          "quick_replies":[
+        {
+          "content_type":"text",
+          "title":"Red",
+          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+        },
+        {
+          "content_type":"text",
+          "title":"Green",
+          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
         }
+      ]
       }
     }
   };
