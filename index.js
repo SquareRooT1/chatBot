1f52a0ee9d4e7c3817e28a900f85d7d236baeddb
SquareRoot1 <m.kanlidere01@gmail.com>
2017-08-01 16:02:33 +0300

    fix

--- index.js
+++ index.js
@@ -122,23 +122,19 @@ function sendGenericMessage(recipientId) {
               type: "postback",
               title: "Onayla",
               payload: "Success for second item",
+            }],
+            quick_replies : [{
+              content_type:"text",
+              title:"Onayla",
+              payload: "Confirm_Item"
+            },{
+              content_type:"text",
+              title:"Reddet",
+              payload: "Refuse_Item"
             }]
           }]
         }
-      },
-      text:"Pick a color:",
-      quick_replies:[
-         {
-           content_type:"text",
-           title:"Red",
-           payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
-         },
-         {
-           content_type:"text",
-           title:"Green",
-           payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
-         }
-       ]  
+      }
     }
 
   };
