d4956d359aa7e3b15789f6e522ae23a8bd59ff70
SquareRoot1 <m.kanlidere01@gmail.com>
2017-08-01 10:55:23 +0300

    fix

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser')
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
