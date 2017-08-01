2c0767192aca77f94ead96c58aa8cf4776b884aa
SquareRoot1 <m.kanlidere01@gmail.com>
2017-08-01 15:58:26 +0300

    fix

--- index.js
+++ index.js
@@ -125,20 +125,20 @@ function sendGenericMessage(recipientId) {
             }]
           }]
         }
-        text:"Pick a color:",
-        quick_replies:[
-           {
-             content_type:"text",
-             title:"Red",
-             payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
-           },
-           {
-             content_type:"text",
-             title:"Green",
-             payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
-           }
-         ]      
-      }
+      },
+      text:"Pick a color:",
+      quick_replies:[
+         {
+           content_type:"text",
+           title:"Red",
+           payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+         },
+         {
+           content_type:"text",
+           title:"Green",
+           payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+         }
+       ]  
     }
 
   };
